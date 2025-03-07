7c8fb774f1acc5a4a03624de3db229b9b2ff9fc4
Youbuu2 <bijan121@verizon.net>
2025-03-06 17:49:17 -0800

    developments

--- RoR2RandCharSelect.py
+++ RoR2RandCharSelect.py
@@ -1,8 +1,6 @@
 import random
+import keyboard
+import pyautogui
 Characters = ["Commando", "Huntress", "Bandit", "MUL-T", "Engineer", "Artificer", "Mercenary", "Rex", "Loader", "Acrid", "Captain", "Railgunner", "Void Fiend", "Seeker", "Chef", "False Son"]
-
 random_character = random.choice(Characters)
-
 print(random_character)
-
-
